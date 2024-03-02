ebacd96c3b479da06595659395ebba4313110110
Slvipking <150326970+Slvipking@users.noreply.github.com>
2024-03-02 20:00:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763415228', 'DARK NAME KING', true],
+  ['94763415228', 'DARK NAME', true], 
   [''] 
 ] //Number of owners
 
